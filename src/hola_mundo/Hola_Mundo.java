e56fab3c4e4187bd0706cd6cb637bcc54318c4ba
Alexis Gardez <Alexis Gardez@Alexis>
2018-05-29 10:36:34 -0600

    hola mundo

---
+++ src/hola_mundo/Hola_Mundo.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hola_mundo;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Alexis Gardez
+ */
+public class Hola_Mundo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      String mensaje= "Hola Mundo";
+      JOptionPane.showMessageDialog(null, mensaje);
+    }
+    
+}
